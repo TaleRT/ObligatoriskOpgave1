aece74a26201f3220597ba21181b49b35bfb5e9e
TaleRT <a.reinhard.tullberg@gmail.com>
2023-10-04 20:13:15 +0200

    Sortering af price og title

--- BooksRepository.cs
+++ BooksRepository.cs
@@ -6,25 +6,40 @@ using System.Threading.Tasks;
 
 namespace ObligatoriskOpgave
 {
-    internal class Book
+    public class Book
     {
         public string Title { get; set; }
         public string Author { get; set; }
+        public int Price { get; set; }
 
-        public Book(string title, string author)
+        public Book(string title, string author, int price)
         {
             this.Title = title;
             this.Author = author;
+            this.Price = price;
         }
     }
     public class BookRepository
     {
         public List<Book> books = new List<Book>();
 
-        public void AddBook(string title, string author)
+        public void AddBook(string title, string author, int price)
         {
-            Book newBook = new Book(title, author);
+            Book newBook = new Book(title, author, price);
             books.Add(newBook);
+        }
+
+        public List<Book> Get(int? filterPrice = null)
+        {
+            if (filterPrice.HasValue)
+            {
+                return books.Where(book => book.Price == filterPrice.Value).ToList();
+
+            }
+            else
+            {
+                return new List<Book>(books);
+            }
         }
 
         public List<Book> Get()
@@ -32,6 +47,29 @@ namespace ObligatoriskOpgave
             // Den her returnere listen af book objekterne
             return new List<Book>(books);
         }
+        public List<Book> Get(string? sortBy = null)
+        {
+            var filteredBooks = new List<Book>(books);
+
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                switch (sortBy.ToLower())
+                {
+                    case "title":
+                        filteredBooks = filteredBooks.OrderBy(book => book.Title).ToList();
+                        break;
+                    case "price":
+                        filteredBooks = filteredBooks.OrderBy(book => book.Price).ToList();
+                        break;
+                    default:
+                        // Handle unsupported sorting criteria
+                        break;
+                }
+            }
+
+            return filteredBooks;
+        }
+
     }
     class Program
     {
@@ -39,20 +77,22 @@ namespace ObligatoriskOpgave
         {
             BookRepository repository = new BookRepository();
 
-            repository.AddBook("Twisted Love", "Ana Huang");
-            repository.AddBook("Uden Håb", "Colleen Hoover");
-            repository.AddBook("Forbandede Kærlighed", "Colleen Hoover");
-            repository.AddBook("Twisted Games", "Ana Huang");
-            repository.AddBook("Manden I Tre Dele", "Thomas Bagger");
+            repository.AddBook("Twisted Love", "Ana Huang", 500);
+            repository.AddBook("Uden Håb", "Colleen Hoover", 500);
+            repository.AddBook("Forbandede Kærlighed", "Colleen Hoover", 500);
+            repository.AddBook("Twisted Games", "Ana Huang", 500);
+            repository.AddBook("Manden I Tre Dele", "Thomas Bagger", 500);
 
             List<Book> allBooks = repository.Get();
 
             foreach (var book in allBooks)
             {
                 Console.WriteLine($"Title: {book.Title}, Fofatter: {book.Author}");
+            }
         }
-    }
-   
 
-    
+
+
+    }
 }
+
